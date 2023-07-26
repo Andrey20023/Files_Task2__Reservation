1d60ba37a1d08f9d9533ddbb5c55c78229b25e99
Andrey20023 <66505241+Andrey20023@users.noreply.github.com>
2023-07-26 18:05:21 +0300

    Add main

---
+++ src/Main.java
@@ -0,0 +1,105 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class Main {
+    public static void main(String[] args) {
+        GameProgress gameProgress1 = new GameProgress(1, 1, 1, 1);
+        GameProgress gameProgress2 = new GameProgress(2, 2, 2, 2);
+        GameProgress gameProgress3 = new GameProgress(3, 3, 3, 3);
+        saveGame("C:\\Games\\savegames\\save1.dat", gameProgress1);
+        saveGame("C:\\Games\\savegames\\save2.dat", gameProgress2);
+        saveGame("C:\\Games\\savegames\\save3.dat", gameProgress3);
+        List<String> listPath = Arrays.asList("C:\\Games\\savegames\\save1.dat", "C:\\Games\\savegames\\save2.dat", "C:\\Games\\savegames\\save3.dat");
+        List<String> listPath2 = Arrays.asList("save1.dat", "save2.dat", "save3.dat");
+
+        //List<String> listPath = Arrays.asList("save1.dat", "save2.dat", "save3.dat");
+        zipFiles("C:\\Games\\savegames\\zip.zip", listPath);
+        delFiles(listPath);
+    }
+
+    public static void saveGame(String path, GameProgress gameProgress) {
+
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(path))) {
+            oos.writeObject(gameProgress);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    private static void zipFiles(String path, List<String> list) { // реализация с получением имени архивируемого файла для ZipEntry через метод getFileName созданного объекта Path
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path))) {
+            for (String arr : list) {
+                Path filePath = Paths.get(arr);
+                try (FileInputStream fis = new FileInputStream(arr)) {
+                    ZipEntry entry = new ZipEntry(filePath.getFileName().toString());
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+    /*private static void zipFiles(String path, List<String> list) { // реализация с получением имени архивируемого файла для ZipEntry через метод getName созданного объекта File
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path))) {
+            for (String arr : list) {
+                File file = new File((arr));
+                try (FileInputStream fis = new FileInputStream(file)) {
+                    if (file.exists()) {
+                        ZipEntry entry = new ZipEntry(file.getName());
+                        zout.putNextEntry(entry);
+                        byte[] buffer = new byte[fis.available()];
+                        fis.read(buffer);
+                        zout.write(buffer);
+                        zout.closeEntry();
+                    }
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }*/
+
+    /*private static void zipFiles(String path, List<String> list) {
+        try (ZipOutputStream zout = new ZipOutputStream(new FileOutputStream(path))) {
+            for (String arr : list) {
+                try (FileInputStream fis = new FileInputStream(arr)) {
+                    ZipEntry entry = new ZipEntry(arr);
+                    zout.putNextEntry(entry);
+                    byte[] buffer = new byte[fis.available()];
+                    fis.read(buffer);
+                    zout.write(buffer);
+                    zout.closeEntry();
+                } catch (Exception ex) {
+                    System.out.println(ex.getMessage());
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }*/
+    private static void delFiles(List<String> list) {
+        for (String arr : list) {
+            File currentFile = new File(arr);
+            if (currentFile.delete()) {
+                System.out.println("Файл " + arr + " успешно удален.");
+            }
+        }
+    }
+}
